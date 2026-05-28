aa9a7fbf43cd7da10648920a2b438f265963d0d4
Clément Schockaert <cschockaert@auchan.fr>
2026-05-28 15:03:03 +0200

    lint: scope nolint on scheme.Builder for controller-runtime 0.24 deprecation

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -12,8 +12,10 @@ var (
 	// GroupVersion is group version used to register these objects
 	GroupVersion = schema.GroupVersion{Group: "slumlord.io", Version: "v1alpha1"}
 
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
+	// The deprecation warning targets API packages meant to be importable as
+	// libraries; this is an operator-internal package so the helper is fine.
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion} //nolint:staticcheck // SA1019
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
